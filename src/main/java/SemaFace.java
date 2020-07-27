2562a9cdf46fa61d4e7863e6c2d58863c91cba4c
samEmi <oemilolo@gitlab.cs.ucl.ac.uk>
2020-07-27 07:16:45 +0100

    Implemented Semaphore Fairness using a Queue of Threads

--- src/main/java/SemaFace.java
+++ src/main/java/SemaFace.java
@@ -1,7 +1,13 @@
+
+// In case I decide to write a different Semaphore implementation
 public interface SemaFace {
 
     void acquire() throws InterruptedException;
     void acquire(int permit) throws InterruptedException;
     void release();
     void release(int permit) throws InterruptedException;
+    int availablePermits();
+    int drainPermits();
+    void reducePermits(int reduction);
+    boolean isFair();
 }
--- src/main/java/Semaphore.java
+++ src/main/java/Semaphore.java
@@ -1,28 +1,38 @@
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Semaphore implements SemaFace {
     private int maxpermits;
     private int permitsLeft;
-    private class Request{
-        int requestedPermits;
-    }
+    private Queue<Request> threadQueue = new LinkedList<Request>();
+    private boolean fair;
+    private class Request{}
     public Semaphore(int permits){
         this.maxpermits = permits;
         this.permitsLeft = permits;
+    }
+
+    public Semaphore(int permits, boolean fair){
+        this(permits);
+        this.fair = fair;
     }
 
 
     public synchronized void acquire() throws InterruptedException {
        acquire(1);
     }
-    public synchronized void acquire(int permit)throws InterruptedException{
+    public synchronized void acquire(int permits)throws InterruptedException{
+        if(permits < 0)throw new IllegalArgumentException();
         if(Thread.interrupted()) throw new InterruptedException();
-        while (permitsLeft - permit < 0){
-            wait();
+        Request request = new Request();
+        if(fair){
+            threadQueue.add(request);
         }
-        permitsLeft = permitsLeft - permit;
+        else{
+            request = null;
+        }
+        checkWait(permits, request);
     }
-
 
     public synchronized void release() {
         release(1);
@@ -35,5 +45,34 @@ public class Semaphore implements SemaFace {
         notifyAll();
     }
 
+    public synchronized int availablePermits(){
+        return permitsLeft;
+    }
 
+    public synchronized int drainPermits(){
+        int result = permitsLeft;
+        permitsLeft = 0;
+        return result;
+    }
+    public synchronized void reducePermits(int reduction){
+        int temp = permitsLeft - reduction;
+        if(temp < 0)throw new IllegalArgumentException();
+        permitsLeft = temp;
+    }
+
+    public synchronized boolean isFair(){
+        return fair;
+    }
+
+    public synchronized String toString(){
+        return this + "[Permits = " + permitsLeft + " ]";
+    }
+
+    private void checkWait(int permits, Request request)throws InterruptedException{
+        while (permitsLeft - permits < 0 && threadQueue.peek() == request){
+            wait();
+        }
+        threadQueue.poll();
+        permitsLeft = permitsLeft - permits;
+    }
 }
